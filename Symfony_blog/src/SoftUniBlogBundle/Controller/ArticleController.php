daf6498a8f9024e5cf9010c593f9f875216e27d5
tpettrov <apetrov@vkp.prb>
2017-04-04 16:32:39 +0300

    changes in article controler

--- Symfony_blog/src/SoftUniBlogBundle/Controller/ArticleController.php
+++ Symfony_blog/src/SoftUniBlogBundle/Controller/ArticleController.php
@@ -2,9 +2,33 @@
 
 namespace SoftUniBlogBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use SoftUniBlogBundle\Entity\Article;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class ArticleController extends Controller
 {
+
+    /**
+     * @param Request $request
+     * @Route("/article/request", name="article_create")
+     *
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     *
+     */
+
+
+    public function create(Request $request){
+
+        $article = new Article();
+
+        $form = $this->createForm(AticleType::class, $article);
+
+            return $this->render('article/create.html.twig', array('form' => $form->createView()));
+
+
+    }
+
 
 }
