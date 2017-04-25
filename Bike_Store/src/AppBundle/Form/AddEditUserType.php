1a21a83f5b5fc10995ffff34d26c528b3aa4a4dd
tpettrov <apetrov@vkp.prb>
2017-04-25 15:07:26 +0300

    security updated for editor

--- Bike_Store/src/AppBundle/Form/AddEditUserType.php
+++ Bike_Store/src/AppBundle/Form/AddEditUserType.php
@@ -6,7 +6,6 @@ use AppBundle\Entity\Role;
 use AppBundle\Entity\User;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\MoneyType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
